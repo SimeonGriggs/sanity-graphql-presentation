40e812cb3a1f28b26f91f162d136e9771b187e2d
Simeon Griggs <simeon@hey.com>
2024-01-05 10:23:40 +0000

    update deps and add debug messages

--- remix/app/components/Preferences.tsx
+++ remix/app/components/Preferences.tsx
@@ -9,28 +9,40 @@ export function Preferences(props: { preferences: PreferencesCookie }) {
       action="/resources/preferences"
       className="container mx-auto flex justify-center gap-2 p-12"
     >
-      <button
-        type="submit"
-        name="perspective"
-        value={
-          props.preferences.perspective === "previewDrafts"
-            ? "published"
-            : "previewDrafts"
-        }
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Preview Drafts:{` `}
-        {props.preferences.perspective === "previewDrafts" ? `On` : `Off`}
-      </button>
-      <button
-        type="submit"
-        name="overlays"
-        value={props.preferences.overlays === "on" ? "off" : "on"}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Visual Editing:{` `}
-        {props.preferences.overlays === "on" ? `On` : `Off`}
-      </button>
+      <div className="flex flex-col gap-4 w-64">
+        <button
+          type="submit"
+          name="perspective"
+          value={
+            props.preferences.perspective === "previewDrafts"
+              ? "published"
+              : "previewDrafts"
+          }
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Preview Drafts:{` `}
+          {props.preferences.perspective === "previewDrafts" ? `On` : `Off`}
+        </button>
+        <span className="text-sm text-balance">
+          This button should not exist because the perspective should be
+          intercepted from Presentation tool
+        </span>
+      </div>
+      <div className="flex flex-col gap-4 w-64">
+        <button
+          type="submit"
+          name="overlays"
+          value={props.preferences.overlays === "on" ? "off" : "on"}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Visual Editing:{` `}
+          {props.preferences.overlays === "on" ? `On` : `Off`}
+        </button>
+        <span className="text-sm text-balance">
+          This button is only here for my debug purposes, ideally Visual Editing
+          should be on when viewed inside Presentation, and off in Production.
+        </span>
+      </div>
     </form>
   );
 }
